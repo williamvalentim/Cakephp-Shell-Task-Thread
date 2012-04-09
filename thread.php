2e8900bba24ec0772938aea9667e306a9b693a6d
William Dorea Valentim <williamvalentim@gmail.com>
2012-04-09 16:43:35 -0300

    Desenvolvimento da Task que dispara jobs e o controla a quantidade de processos que podem ser executados simultaneamente

---
+++ thread.php
@@ -0,0 +1,43 @@
+<?php
+
+class ThreadTask extends Shell
+{
+	public $pids = array();
+	public $processLimit = 20;
+	public $secondsWait = 5;
+	
+	public function processExec($commandJob) { 
+        $command = $commandJob.' > /tmp/consolidacao.log 2>&1 & echo $!';
+        exec($command ,$op); 
+        $pid = (int)$op[0]; 
+        if ($pid!="") {
+	        return $pid; 
+        }
+        return false; 
+    } 
+
+    public function checkLimitProcess() { 
+		foreach ($this->pids as $key => $pid) {
+	        exec("ps ax | grep $pid 2>&1", $output);
+	        $runningProcess = false;
+	        while( list(,$row) = each($output) ) {
+                $row_array = explode(" ", $row); 
+                $check_pid = $row_array[0];
+                if($pid == $check_pid) {
+                	$runningProcess = true; 
+                	break 2;
+                }
+	        }
+			if (!$runningProcess) {
+				unset($this->pids[$key]);
+			}
+		}
+		
+		if (count($this->pids) >= $this->processLimit) {
+			$this->log("Wait : Count process running: ".count($this->pids), true);
+			sleep($this->secondsWait);
+			$this->checkLimitProcess();
+		}
+		return true;
+    } 
+}
